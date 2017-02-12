9aa891840e90a9c6fe9a3aa13a6203dc8849c41b
fannyelvira <fanny.sandblom@gmail.com>
2017-02-12 16:35:47 +0100

    renamed package and changed msg types to real ackermann msg and std_msg int64

---
+++ scripts/pid.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import sys
+#from hw_api_ackermann.msg import AckermannDrive
+#from gulliviewServer.msg import Num
+from ackermann_msgs.msg import AckermannDrive
+from std_msgs.msg import Int64
+
+class pid:
+
+    def __init__(self):
+        print "init"
+        self.speed = 0.5
+        self.steering_angle = 0
+        self.last_time = 0.0
+        self.last_error = 0
+        self.int = 0
+
+        # Parameters
+        self.seta = 0.6
+        self.wn = 1.3
+        self.delta = 5
+        self.lh = 0.21
+
+        #self.pub = rospy.Publisher("auto_drive", AckermannDrive, queue_size=10)
+        #self.sub = rospy.Subscriber("error", Num, self.callback)
+        self.pub = rospy.Publisher("auto_drive", AckermannDrive, queue_size=10)
+        self.sub = rospy.Subscriber("error", Int64, self.callback)
+        print "waiting for input"
+
+    def callback(self, data):
+        self.loop(data)
+
+    def loop(self, data):
+        error = data.num
+
+        # Calculating dt
+        time = rospy.get_time()
+        #time = data.header.stamp.secs + data.header.stamp.nsecs / 1000000000.0
+        dt = time - self.last_time
+        #if dt > 10:
+        #    dt = 0
+        self.last_time = time
+
+        # Designing equations
+        Ki = self.delta / self.wn
+        p = (2 * self.seta * self.wn + self.speed) / (1 - self.delta * self.wn)
+        Km = -(self.speed**2) / self.lh
+        Kp = (-self.delta * self.wn * p) / Km
+        Kd = (self.wn**2 - 2 * self.seta * self.wn * p) / (Kp*Km)
+
+        self.int = self.int + error * dt
+
+        # anti-windup
+        if self.int > 0.4 :
+            self.int = 0.4
+        if self.int < -0.4:
+            self.int = -0.4
+
+        dev = (error - self.last_error) / dt
+        self.steering_angle = Kp * error + Ki * self.int + Kd * dev
+
+        self.last_error = error
+
+        # Construct the message
+        ack = AckermannDrive()
+        ack.steering_angle = self.steering_angle
+        ack.speed = self.speed
+
+        self.pub.publish(ack)
+
+
+def main(args):
+
+    rospy.init_node('pid', anonymous=True)
+    pid()
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+ 
+if __name__ == '__main__':
+    main(sys.argv)
