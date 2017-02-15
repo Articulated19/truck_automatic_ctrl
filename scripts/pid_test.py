78b863e6e1815ef226ccf4f94ebb478844a034f6
seholm <filipslottner@gmail.com>
2017-02-15 20:26:48 +0100

    added test stub

---
+++ scripts/pid_test.py
@@ -0,0 +1,12 @@
+import unittest
+#from pid import *
+
+class TestPid(unittest.TestCase):
+    def test_pid_method1(self):
+        self.assertTrue(True)
+
+    def test_pid_method2(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
