c5545de35b5281e727dba94b5353162604132a2d
Lars Niklasson <larsniklasson94@gmail.com>
2017-02-17 19:48:01 +0100

    refactor pid

--- scripts/pid.py
+++ scripts/pid.py
@@ -1,101 +1,54 @@
-#!/usr/bin/env python
-# license removed for brevity
-import rospy
-import sys
-from ackermann_msgs.msg import AckermannDrive
-from std_msgs.msg import Int64
+#!/usr/bin/python
 
 
-PUBLISH_TOPIC = "auto_drive"
-SUB_TOPIC = "error"
+import time
 
-#MAX_STEERING = ?
-#MIN_STEERING = ?
-#ZERO_STEERING = ?
+class PID:
 
+    def __init__(self, P, I, D):
 
-class Pid:
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
 
-    def __init__(self):
-        rospy.loginfo("init")
+        self.ITerm = 0
+
+        self.current_time = time.time()
+        self.last_time = self.current_time
+        self.windup_guard = 0.4
+
+        self.last_error = 0.0
+        self.clear()
+
+    def clear(self):
+        self.ITerm = 0.0
         
 
-        self.speed = 0.6
-        self.steering_angle = 0
-        self.last_time = 0.0
-        self.last_error = 0
-        self.int = 0
-
-        # Parameters
-        self.seta = rospy.get_param('pid/seta', 0.1)
-        self.wn = rospy.get_param('pid/wn', 0.3)
-        self.delta = rospy.get_param('pid/delta', 50.0)
-        self.lh = rospy.get_param('pid/lh', 0.27)
-
-        self.pub = rospy.Publisher(PUBLISH_TOPIC, AckermannDrive, queue_size=10)
-        self.sub = rospy.Subscriber(SUB_TOPIC, Int64, self.callback)
+    def update(self, error, dt = -1):
         
+        current_time = time.time()
+        if dt == -1:
+            delta_time = self.current_time - self.last_time
+        else:
+            delta_time = dt
+        delta_error = error - self.last_error
 
-    def callback(self, data):
-        self.loop(data)
 
-    def loop(self, data):
-        error = data.data/1000.0
-        #rospy.loginfo("received error: %i", error)
+        PTerm = self.Kp * error
+        self.ITerm += error * delta_time
 
-        # Calculating dt
-        time = rospy.get_time()
-        #time = data.header.stamp.secs + data.header.stamp.nsecs / 1000000000.0
-        dt = time - self.last_time
-        #if dt > 10:
-        #    dt = 0
-        self.last_time = time
-        rospy.loginfo("PID: dt: %s", dt)
+        if (self.ITerm < -self.windup_guard):
+            self.ITerm = -self.windup_guard
+        elif (self.ITerm > self.windup_guard):
+            self.ITerm = self.windup_guard
 
-        # Designing equations
-        #Ki = (self.delta / self.wn) * 1
-        Ki = 1.1946
-        Kp = 4.52
-        Kd = 4.2729
-        p = (self.wn**2 + self.speed) / (2 * self.seta * self.wn - self.delta * self.wn)
-        Km = -(self.speed**2) / self.lh
-        #Kp = (-self.delta * self.wn * p) / Km
-        #Kd = ((2 * self.seta * self.wn - p) / (Kp * Km)) * 1
-        rospy.loginfo("ki %s, kp %s, kd %s", Ki, Kp, Kd)
+        DTerm = 0.0
+        if delta_time > 0:
+            DTerm = delta_error / delta_time
 
-        self.int = self.int + error * dt
-        print self.int
-
-        # anti-windup
-        if self.int > 0.4 :
-            self.int = 0.4
-        if self.int < -0.4:
-            self.int = -0.4
-
-        dev = (error - self.last_error) / dt
-        self.steering_angle = Kp * error + Ki * self.int - Kp* Kd * dev
-        #self.steering_angle = Kp * error + (1 / Ki * self.int - Kd * dev) * Kp
-	#self.steering_angle = Kp * error + 1  
-
+        # Remember last time and last error for next calculation
+        self.last_time = current_time
         self.last_error = error
 
-        # Construct the message
-        ack = AckermannDrive()
-        ack.steering_angle = self.steering_angle
-        ack.speed = self.speed
+        return PTerm + (self.Ki * self.ITerm) + (self.Kd * DTerm)
 
-        self.pub.publish(ack)
-
-def main(args):
-    rospy.init_node('pid', anonymous=True)
-    Pid()
-    try:
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
- 
-if __name__ == '__main__':
-    main(sys.argv)
-
-
-# To manually publish messages on 'error' topic: $ rostopic pub /error std_msgs/Int64 -- [value]
